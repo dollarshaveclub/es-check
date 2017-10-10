f7455a2439705729d8e12c2e0666cbbf22b9fd84
Jeff Wainwright <yowainwright@gmail.com>
2017-10-10 11:17:33 -0700

    [update] changes messaging slightly + test paths

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const argsArray = process.argv
 prog
   .version(pkg.version)
   .command('check')
+  .alias('c')
   .argument(
     '<ecma-version>',
     'define the EcmaScript version to check for against a glob of JavaScript files'
@@ -30,37 +31,39 @@ prog
     let e
     // define ecmaScript version
     switch (v) {
-    case 'ecma3':
-      e = '3'
-      break
-    case 'ecma4':
-      e = '4'
-      break
-    case 'ecma6':
-      e = '6'
-      break
-    case 'ecma7':
-      e = '7'
-      break
-    case 'ecma8':
-      e = '8'
-      break
-    default:
-      e = '5'
+      case 'ecma3':
+        e = '3'
+        break
+      case 'ecma4':
+        e = '4'
+        break
+      case 'ecma6':
+        e = '6'
+        break
+      case 'ecma7':
+        e = '7'
+        break
+      case 'ecma8':
+        e = '8'
+        break
+      default:
+        e = '5'
     }
 
     const errors = []
 
     files.forEach((pattern) => {
-      const globbedFiles = glob.sync(pattern, options)
+      // pattern => glob or array
+      const globbedFiles = glob.sync(pattern)
 
-      globbedFiles.forEach(function (file) {
+      globbedFiles.forEach((file) => {
         const code = fs.readFileSync(file, 'utf8')
         try {
           const result = acorn.parse(code, {
-              ecmaVersion: e
+            ecmaVersion: e,
+            silent: true,
           })
-          logger.info(`Ecma-v: '${file}' matches ecma${e}`)
+          logger.info(`🏆 Ecma-v: '${file}' matches ecma${e}`)
         } catch (err) {
           logger.error(`Ecma-v: '${file}' does not match ecma${e}`)
           errors.push(file)
@@ -69,9 +72,14 @@ prog
     })
 
     if (errors.length > 0) {
-      logger.error(`\n🏆  Ecma-v: there were ${errors.length} matching errors!`)
+      logger.error(`Ecma-v: there were ${errors.length} matching errors!`)
+      errors.forEach((error) => {
+        logger.info(`\n Ecma-v: error in: ${error}`)
+      })
       process.exit(1)
       return
+    } else {
+      logger.error(`Ecma-v: there were no matching errors! 🎉`)
     }
   })
 
