6ffdcea60f93f87c5daee203d538ba2455229474
Jeff Wainwright <yowainwright@gmail.com>
2017-10-11 10:53:13 -0700

    [init] new name

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec
 const globby = require('globby')
 const assert = require('assert')
 
-it('🎉  Ecma V should pass when checking an array of es5 files as es5', (done) => {
+it('🎉  Es Check should pass when checking an array of es5 files as es5', (done) => {
   exec('node index.js check ecma5 ./tests/es5.js ./tests/es5-2.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
@@ -15,7 +15,7 @@ it('🎉  Ecma V should pass when checking an array of es5 files as es5', (done)
   })
 })
 
-it('👌  Ecma V should fail when checking an array of es6 files as es5', (done) => {
+it('👌  Es Check should fail when checking an array of es6 files as es5', (done) => {
   exec('node index.js check ecma5 ./tests/es6.js ./tests/es6-2.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
@@ -23,7 +23,7 @@ it('👌  Ecma V should fail when checking an array of es6 files as es5', (done)
   })
 })
 
-it('🎉  Ecma V should pass when checking a glob of es6 files ', (done) => {
+it('🎉  Es Check should pass when checking a glob of es6 files ', (done) => {
   exec('node index.js check ecma6 ./tests/*.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
@@ -36,7 +36,7 @@ it('🎉  Ecma V should pass when checking a glob of es6 files ', (done) => {
   })
 })
 
-it('👌  Ecma V should fail when checking a glob of es5 files', (done) => {
+it('👌  Es Check should fail when checking a glob of es5 files', (done) => {
   exec('node index.js check ecma5 ./tests/*.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
