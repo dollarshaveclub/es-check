a5258fbaa426a4d668c8765db52aa915f8a714ab
Jeff Wainwright <yowainwright@gmail.com>
2017-10-11 17:05:49 -0700

    [update] updates readme for the new world

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const globby = require('globby')
 const assert = require('assert')
 
 it('🎉  Es Check should pass when checking an array of es5 files as es5', (done) => {
-  exec('node index.js check ecma5 ./tests/es5.js ./tests/es5-2.js', (err, stdout, stderr) => {
+  exec('node index.js check es5 ./tests/es5.js ./tests/es5-2.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
       console.error(stdout.toString())
@@ -16,7 +16,7 @@ it('🎉  Es Check should pass when checking an array of es5 files as es5', (don
 })
 
 it('👌  Es Check should fail when checking an array of es6 files as es5', (done) => {
-  exec('node index.js check ecma5 ./tests/es6.js ./tests/es6-2.js', (err, stdout, stderr) => {
+  exec('node index.js check es5 ./tests/es6.js ./tests/es6-2.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
     done()
@@ -24,7 +24,7 @@ it('👌  Es Check should fail when checking an array of es6 files as es5', (don
 })
 
 it('🎉  Es Check should pass when checking a glob of es6 files ', (done) => {
-  exec('node index.js check ecma6 ./tests/*.js', (err, stdout, stderr) => {
+  exec('node index.js check es6 ./tests/*.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
       console.error(stdout.toString())
@@ -37,7 +37,7 @@ it('🎉  Es Check should pass when checking a glob of es6 files ', (done) => {
 })
 
 it('👌  Es Check should fail when checking a glob of es5 files', (done) => {
-  exec('node index.js check ecma5 ./tests/*.js', (err, stdout, stderr) => {
+  exec('node index.js check es5 ./tests/*.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
     done()
