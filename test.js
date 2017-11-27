cb494bb1b129a419e6932939705da5e9349c7f7d
Brian Gonzalez <me@briangonzalez.org>
2017-11-27 11:14:10 -0800

    fixed tests

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec
 const assert = require('assert')
 
 it('🎉  Es Check should pass when checking an array of es5 files as es5', (done) => {
-  exec('node index.js check es5 ./tests/es5.js ./tests/es5-2.js', (err, stdout, stderr) => {
+  exec('node index.js es5 ./tests/es5.js ./tests/es5-2.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
       console.error(stdout.toString())
@@ -17,7 +17,7 @@ it('🎉  Es Check should pass when checking an array of es5 files as es5', (don
 })
 
 it('👌  Es Check should fail when checking an array of es6 files as es5', (done) => {
-  exec('node index.js check es5 ./tests/es6.js ./tests/es6-2.js', (err, stdout, stderr) => {
+  exec('node index.js es5 ./tests/es6.js ./tests/es6-2.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
     done()
@@ -25,7 +25,7 @@ it('👌  Es Check should fail when checking an array of es6 files as es5', (don
 })
 
 it('🎉  Es Check should pass when checking a glob of es6 files ', (done) => {
-  exec('node index.js check es6 ./tests/*.js', (err, stdout, stderr) => {
+  exec('node index.js es6 ./tests/*.js', (err, stdout, stderr) => {
     if (err) {
       console.error(err.stack)
       console.error(stdout.toString())
@@ -38,7 +38,7 @@ it('🎉  Es Check should pass when checking a glob of es6 files ', (done) => {
 })
 
 it('👌  Es Check should fail when checking a glob of es5 files', (done) => {
-  exec('node index.js check es5 ./tests/*.js', (err, stdout, stderr) => {
+  exec('node index.js es5 ./tests/*.js', (err, stdout, stderr) => {
     assert(err)
     console.log(stdout)
     done()
